61f0bf1cad96fe8654b1c0aac8b3629d2ab48fdc
mohammed ismail <warethco@gmail.com>
2023-01-05 09:36:47 -0800

    some fix

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        from calculator_1 import add, sub, mul, div
+            import sys
+                ops = "+-*/"
+                    argvlen = len(sys.argv)
+                        if argvlen != 4:
+                                    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+                                            exit(1)
+
+                                                args = sys.argv
+                                                    a = args[1]
+                                                        operator = args[2]
+                                                            b = args[3]
+                                                                if operator not in ops:
+                                                                            print("Unknown operator. Available operators: +, -, * and /")
+                                                                                    exit(1)
+                                                                                        if (operator == "+"):
+                                                                                                    print("{} + {} = {}".format(a, b, add(a, b)))
+                                                                                        elif (operator == "-"):
+                                                                                                    print("{} - {} = {}".format(a, b, sub(a, b)))
+                                                                                        elif (operator == "*"):
+                                                                                                    print("{} * {} = {}".format(a, b, mul(a, b)))
+                                                                                        else:
+                                                                                                    print("{} / {} = {}".format(a, b, div(a, b)))
