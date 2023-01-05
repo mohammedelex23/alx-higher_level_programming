8162a8461d1227ce7499d07d59a28be398978806
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-05 18:30:58 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-        import sys
-            argvlen = len(sys.argv)
-                if argvlen == 1:
-                            print(0)
-                else:
-                            args = sys.argv
-                                    sum = 0
-                                            for index in range(1, len(args)):
-                                                            arg = int(args[index])
-                                                                        sum += arg
-                                                                                print(sum)
-                                                                                
+    import sys
+    argvlen = len(sys.argv)
+    if argvlen == 1:
+        print(0)
+    else:
+        args = sys.argv
+        sum = 0
+        for index in range(1, len(args)):
+            arg = int(args[index])
+            sum += arg
+        print(sum)
