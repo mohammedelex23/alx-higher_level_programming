d0bcfbcb34094d82098ecf761f1de7290b4aeabc
mohammed ismail <warethco@gmail.com>
2023-01-05 08:30:34 -0800

    some fix

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+        import sys
+            argvlen = len(sys.argv)
+                if argvlen == 1:
+                            print(0)
+                else:
+                            args = sys.argv
+                                    sum = 0
+                                            for index in range(1, len(args)):
+                                                            arg = int(args[index])
+                                                                        sum += arg
+                                                                                print(sum)
+                                                                                
