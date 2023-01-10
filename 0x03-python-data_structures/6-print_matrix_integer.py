4fc51289280a9b7835a5602aed0ebddb1a1c0a01
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-10 09:33:06 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            end = " "
-            if row.index(col) == len(row) - 1:
-                end = "$"    
-            print("{:d}".format(col),end = end)
-        print("\n")
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print('$')
+    else:
+        for row in range(len(matrix)):
+            for item in range(len(matrix[row])):
+                if item != len(matrix[row]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = '$'
+                print("{:d}".format(matrix[row][item]), end=endspace)
+            print()
