60e376c03acb0226cf8d25e7f4e38cec6678ad44
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-10 11:44:04 +0200

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,4 @@
 def multiple_returns(sentence):
     if not sentence or len(sentence) == 0:
         return tuple((0, None))
-    return (len(sentence),sentence[0])
+    return (len(sentence), sentence[0])
