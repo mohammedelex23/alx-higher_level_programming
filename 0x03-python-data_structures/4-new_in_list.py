42c3aca29599fec474bb090d7a861ed9e67e9975
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-10 09:06:01 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
-        if idx in range(0,len(my_list)):
-            new_list[idx] = element
-    return new_list
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    temp_list = list(my_list)
+    temp_list[idx] = element
+    return temp_list
