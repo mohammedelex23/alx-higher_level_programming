6d1f2994948df08b894f50f81af5be67c5d00696
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-11 16:27:31 +0200

    Update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    matrix_copy = [el.copy() for el in matrix]
-    return list(map(lambda r: list(map(lambda c: c ** 2, r)), matrix_copy))
+    return list(map(lambda r: list(map(lambda c: c ** 2, r)), matrix))
