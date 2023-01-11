1cff79c0b814b753a33f42199e4253e16f91553b
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-11 14:53:25 +0200

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+    values = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 }
+    sum = 0
+    length = len(roman_string)
+    for i in range(0, length):
+        first = roman_string[i]
+        second = roman_string[i]
+        if (i + 1) < len(roman_string):
+            second = roman_string[i + 1]
+        if values[first] < values[second]:
+            sum += -values[first]
+        else:
+            sum += values[first]
+    return sum
