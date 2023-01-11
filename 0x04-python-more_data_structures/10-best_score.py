bdde9d18346c1a9fe81fe7750889b2eada81ae44
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-11 14:00:57 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@ def best_score(a_dictionary):
         return None
     best = 0
     for key in a_dictionary:
-        if a_dictionary[key] >= best:
+        if a_dictionary[key] > best:
             best = a_dictionary[key]
     return best
