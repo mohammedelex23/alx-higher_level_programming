fc9f973c3c30598ae75cb9072fbdfaefd79c9461
Mohammed Ismail Mohammed <62588958+mohammedelex23@users.noreply.github.com>
2023-01-11 16:14:24 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,15 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    values = {"I": 1, "V": 5, "X": 10, "L": 50,
-               "C": 100, "D": 500, "M": 1000}
+    values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     sum = 0
     length = len(roman_string)
     for i in range(0, length):
